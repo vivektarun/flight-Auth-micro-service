8b88d16de4451954df85f6d8de0b5183a36ca61e
vivektarun <vivektarun1234@gmail.com>
2025-08-05 01:16:00 +0530

    feat: implement rate-limiter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
-
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes'); // By default index.js in required.
+const ratelimit = require('express-rate-limit');
 
 const app = express();
+
+const limiter = ratelimit.rateLimit({
+    windowMs: 2 * 60 * 1000, // 2 min
+    max: 3, //Maximum 3 request per 2 minute.
+});
+
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(express.text());
+app.use(limiter);
 
 app.get('/', (req, res) => {
     res.send("service is up");
